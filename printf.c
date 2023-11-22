ca62873b2e552573391d6a45dfe9e97615cf21b0
Eche <echyth@yahoo.com>
2023-11-22 14:00:54 +1000

    implement dummy _printf structure

--- printf.c
+++ printf.c
@@ -2,25 +2,57 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 
 int _printf(const char *const fmt, ...)
 {
-	/*va_list args;*/
+	va_list args;
 	size_t index;
 	char ch;
+	float fval;
+	int cval, dval;
+	char *sval;
 
 	index = 0;
 
 	if (!fmt)
 		return EXIT_FAILURE;
 
-	while ((ch = fmt[index]))
+	va_start(args, fmt);
+
+	for (index = 0; fmt[index]; index++)
 	{
+		ch = fmt[index];
+
 		if (ch != '%')
 		{
-			putchar(ch);
+			putchar(fmt[index]);
+			continue;
 		}
+
 		index++;
+		switch (fmt[index])
+		{
+		case 'c':
+			cval = va_arg(args, int);
+			printf("%c", cval);
+			break;
+		case 'd':
+			dval = va_arg(args, int);
+			printf("%d", dval);
+			break;
+		case 'f':
+			fval = va_arg(args, double);
+			printf("%f", fval);
+			break;
+		case 's':
+			sval = va_arg(args, char *);
+			printf("%s", sval);
+			break;
+		default:
+			putchar(fmt[index]);
+		}
 	}
+	va_end(args);
 	return EXIT_SUCCESS;
 }
