8f55eab1515f7abab1b50cab601e68a8b1f2e01c
tingru0515 <7687@holbertonstudents.com>
2023-11-23 12:56:54 -0800

    fixed: betty doc print_%.c

--- print_%.c
+++ print_%.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
-* print_37 - prints % signe
-* Return: int
+* print_percent - prints the percent sign '%'
+* Return: the number of characters printed (always 1)
 */
 
-int print_37(void)
+int print_percent(void)
 {
-	_putchar(37);
+	_putchar(37);/* 37 is the ASCII code for '%' */
 	return (1);
 }
