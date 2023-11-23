4e874d1ab50eb76abd731d473e754a3c453fa51e
tingru0515 <7687@holbertonstudents.com>
2023-11-22 22:37:33 -0800

    fixed: Betty style error

--- print_d.c
+++ print_d.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 #include "main.h"
 
-/** printf_integer_recursive - prints a single or multi digit integer
+/**
+* printf_integer_recursive - prints a single or multi digit integer
 * @num: a single or multi digit integer
 * Return: The length of the integer
 */
@@ -19,21 +20,23 @@ int print_integer_recursive(int num)
 		int len = print_integer_recursive(num / 10);
 
 		_putchar(num % 10 + '0');
-		return len + 1;
+		return (len + 1);
 	}
 	else
 	{
 		_putchar(num % 10 + '0');
-		return 1;
+		return (1);
 	}
 }
 
-/* printf_integer - prints an integer
- * @val: arguments
- * Return: the length of the integer
- */
+/**
+* printf_integer - prints an integer
+* @val: arguments
+* Return: the length of the integer
+*/
 int printf_integer(va_list val)
 {
 	int num = va_arg(val, int);
-	return print_integer_recursive(num);
+
+	return (print_integer_recursive(num));
 }
