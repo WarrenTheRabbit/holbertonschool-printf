07405ff75932cbf7abf042f400953dbfbb2933af
tingru0515 <7687@holbertonstudents.com>
2023-11-23 16:28:20 -0800

    delete unused functions of main.h

--- main.h
+++ main.h
@@ -19,9 +19,6 @@ typedef struct
 	int (*print)(va_list args);
 } Formatter;
 int handle_char(va_list args);
-int handle_integer(va_list args);
-int handle_string(va_list args);
-int handle_format_specifier(const char *fmt, size_t *index, va_list args);
 void initialise_formatters(Formatter *specification);
 
 #endif
